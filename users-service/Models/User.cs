5556140ff37b7c1f29d8b81bfd255248660123be
Carlos Radtke <carlos.radtke.a@gmail.com>
2025-01-07 17:04:37 -0300

    Added User Model

---
+++ users-service/Models/User.cs
@@ -0,0 +1,11 @@
+namespace UsersService.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Rut { get; set; }
+        public string? Email { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
